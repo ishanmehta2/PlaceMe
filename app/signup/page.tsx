e17f0ed4acf9dc50fe13df6c5a8b9eb5dace4885
Nils-Forstall <nilzforstall@gmail.com>
2025-06-05 13:33:25 -0700

    removed profile photo in signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -200,41 +200,6 @@ export default function SignUp() {
             />
           </div>
           
-          <div className="space-y-4 mt-2">
-            <label className="block text-4xl font-black" style={{ 
-              fontFamily: 'Arial, sans-serif'
-            }}>
-              Upload a Selfie
-            </label>
-            
-            <div className="flex justify-center">
-              <label htmlFor="selfie" className="cursor-pointer">
-                <div className="w-24 h-24 bg-gray-300 rounded-full flex items-center justify-center">
-                  {previewUrl ? (
-                    <img 
-                      src={previewUrl} 
-                      alt="Preview" 
-                      className="w-full h-full object-cover rounded-full"
-                    />
-                  ) : (
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
-                  )}
-                </div>
-                <input
-                  id="selfie"
-                  name="selfie"
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  className="hidden"
-                />
-              </label>
-            </div>
-          </div>
-          
           <div className="flex justify-center mt-6">
             <button
               type="submit"
